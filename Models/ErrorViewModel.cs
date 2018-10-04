3fab21a70e919353dd6c8e26d85b0324002eb314
Lazaro Fernandes <lazaro.fernandes@lambda3.com.br>
2018-10-04 10:16:10 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mackenzie_workshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+"# mackenzie-aspnetmvc-lazaro"  git init git add README.md git commit -m "first commit" git remote add origin https://github.com/lazarofl/mackenzie-aspnetmvc-lazaro.git git push -u origin master
+"# mackenzie-aspnetmvc-lazaro" 
